1cff22cc094834b14c2549581ef16d723b250a2c
Mattcazz <matteocazzola01@gmail.com>
2026-04-08 22:51:55 +0200

    feat: png para los bistroCoins

--- includes/vistas/common/header.php
+++ includes/vistas/common/header.php
@@ -37,12 +37,7 @@ if (
             <?php if ($saldoCliente !== null): ?>
                 <div class="saldo-cliente" title="Saldo actual en BistroCoins">
                     <span class="moneda-bistro" aria-hidden="true">
-                        <svg viewBox="0 0 24 24" focusable="false" role="img">
-                            <circle cx="12" cy="12" r="8.5"></circle>
-                            <path d="M10.2 8.1h2.5a2 2 0 1 1 0 4h-1.1a2 2 0 1 0 0 4h2.6"></path>
-                            <line x1="10.9" y1="7" x2="10.9" y2="9"></line>
-                            <line x1="13.1" y1="15" x2="13.1" y2="17"></line>
-                        </svg>
+                        <img src="<?= RUTA_IMGS . '/bistroCoins.png' ?>" alt="BistroCoins">
                     </span>
                     <span class="saldo-valor"><?= number_format($saldoCliente, 0, ',', '.') ?></span>
                 </div>
