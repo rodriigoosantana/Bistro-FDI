b7152c8661d0512c2d9ed6e52195e47bc8425454
Mattcazz <matteocazzola01@gmail.com>
2026-04-30 19:12:29 +0200

    feat: imagenes de productos en pedidos

--- includes/vistas/pedidos/pedidosadd.php
+++ includes/vistas/pedidos/pedidosadd.php
@@ -109,6 +109,34 @@ $calcularCanje = function (array $carrito, array $canjes, array $recompensasProd
     return [$costeBistrocoins, $descuentoCanje];
 };
 
+$generarHtmlImagenProducto = function (int $productoId, string $nombreProducto): string {
+    $imagenes = ProductoService::listarImagenes($productoId);
+
+    if (!$imagenes) {
+        return '<div class="tarjeta-sin-imagen"><em>Sin imagen</em></div>';
+    }
+
+    $primeraRuta = htmlspecialchars(RUTA_APP . $imagenes[0]['ruta_imagen']);
+
+    if (count($imagenes) === 1) {
+        return "<img class=\"tarjeta-img-unica\" src=\"{$primeraRuta}\" alt=\"{$nombreProducto}\">";
+    }
+
+    $rutas = array_map(fn($img) => htmlspecialchars(RUTA_APP . $img['ruta_imagen']), $imagenes);
+    $dataImagenes = htmlspecialchars(json_encode($rutas));
+
+    $dotsHtml = '';
+    foreach ($imagenes as $i => $img) {
+        $active = $i === 0 ? ' active' : '';
+        $dotsHtml .= "<span class=\"slider-dot{$active}\"></span>";
+    }
+
+    return '<div class="slider-wrap tarjeta-slider" data-imagenes="' . $dataImagenes . '" data-auto="true">'
+        . '<img class="slider-img" src="' . $primeraRuta . '" alt="' . $nombreProducto . '">' 
+        . '<div class="slider-dots">' . $dotsHtml . '</div>'
+        . '</div>';
+};
+
 $clienteIdPedidoActual = $idPedido ? intval($pedido->getClienteId()) : intval(Aplicacion::getUserId());
 $usuarioSaldoActual = UsuarioService::buscarPorId($clienteIdPedidoActual);
 $saldoBistrocoinsCliente = ($clienteIdPedidoActual === intval(Aplicacion::getUserId()) && isset($_SESSION['saldo']))
@@ -563,9 +591,11 @@ foreach ($listaCategorias as $categoria) {
             $idProducto = $producto->getId();
             $nombreProducto = htmlspecialchars($producto->getNombre());
             $precioFormateado = number_format($producto->getPrecioFinal(), 2, ',', '.') . " €";
+            $htmlImagenProducto = $generarHtmlImagenProducto($idProducto, $nombreProducto);
 
             $htmlProductosCategoria .= <<<HTML
             <div class="producto-card">
+                <div class="tarjeta-imagen">{$htmlImagenProducto}</div>
                 <div class="producto-info">
                     <strong>{$nombreProducto}</strong>
                     <span class="precio">{$precioFormateado}</span>
